d43bebfbaa983116d8d780f40c651dc994421751
aaa <hoonrara2@gmail.com>
2025-03-26 14:16:29 +0900

    lombok 적용 및 코드 간결화

--- src/main/java/com/example/kh/controller/FavoriteMovieController.java
+++ src/main/java/com/example/kh/controller/FavoriteMovieController.java
@@ -3,6 +3,7 @@ package com.example.kh.controller;
 
 import com.example.kh.model.FavoriteMovie;
 import com.example.kh.repository.FavoriteMovieRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,13 +11,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/favorites")
 @CrossOrigin
+@RequiredArgsConstructor
 public class FavoriteMovieController {
 
     private final FavoriteMovieRepository repo;
 
-    public FavoriteMovieController(FavoriteMovieRepository repo) {
-        this.repo = repo;
-    }
+
 
     @GetMapping
     public List<FavoriteMovie> all() {
--- src/main/java/com/example/kh/controller/WeatherController.java
+++ src/main/java/com/example/kh/controller/WeatherController.java
@@ -2,22 +2,21 @@ package com.example.kh.controller;
 
 import com.example.kh.service.TmdbService;
 import com.example.kh.service.WeatherService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
+@RequiredArgsConstructor
 public class WeatherController {
 
 
     private final WeatherService weatherService;
     private final TmdbService tmdbService;
 
-    public WeatherController(WeatherService weatherService, TmdbService tmdbService) {
-        this.weatherService = weatherService;
-        this.tmdbService = tmdbService;
-    }
+
 
     @GetMapping("/recommend")
     public Object recommend(@RequestParam String city) {
